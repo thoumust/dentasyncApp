b771f147d260a09594673a660dcba7c335f82d56
unknown <cit173e-14@M303.UC-BCF.EDU.PH>
2023-10-03 18:34:46 +0800

    intial commit

---
+++ 05_PHP_CONSTANTS.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php
+	//const eg
+	define("MINSIZE", 50);
+	echo MINSIZE;
+	echo constant("MINSIZE");
+
+	// Valid constant names
+	define("ONE", "first thing");
+	define("TWO2", "second thing");
+	define("THREE_3", "third thing");
+
+	?>
+</body>
+</html>
